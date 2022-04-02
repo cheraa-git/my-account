ede549d53d1f75247bc345e13951373ff6c1eebf
cheraa-git <chernetsov.aa@mail.ru>
2022-04-02 16:05:53 +0300

    sort

--- src/components/PersonCardList/PersonCardList.tsx
+++ src/components/PersonCardList/PersonCardList.tsx
@@ -3,6 +3,13 @@ import { Contact } from '../../types/contactsTypes'
 import { PersonCard } from './PersonCard'
 
 export const PersonCardList: React.FC<{ contacts: Contact[] }> = ({ contacts }) => {
+  contacts.sort((a, b) => {
+    if (a.name < b.name) return -1
+    if (a.name > b.name) return 1
+
+    return 0
+  })
+
   return (
     <div>
       {contacts.map((contact, index) => (
